be4e23e8c30ab3d6058cc9ec53c12a5785b2e6ff
Prince <abhisekk141@gmail.com>
2022-10-15 16:10:38 +0530

    gif replaced by single image

--- src/App.js
+++ src/App.js
@@ -28,21 +28,21 @@ function App() {
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/ourworks" element={<ExploreWorks />} >
-          <Route index element={<EatsEzy state={{ title: 'EatsEzy', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-Primary-Colour'
+          <Route index element={<EatsEzy state={{ image:"mobile-mockup.png" ,title: 'EatsEzy', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-Primary-Colour'
             }} />} />
-          <Route path="bigfoot" element={<EatsEzy state={{ title: 'Bigfoot', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-[#D74008]'
+          <Route path="bigfoot" element={<EatsEzy state={{ image:"mobile-mockup.png" ,title: 'Bigfoot', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-[#D74008]'
             }} />} />
-          <Route path="acai" element={<EatsEzy state={{ title: 'Acai', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-[#592D80]'
+          <Route path="acai" element={<EatsEzy state={{ image:"mobile-mockup.png" ,title: 'Acai', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-[#592D80]'
             }} />} />
-          <Route path="legate" element={<EatsEzy state={{ title: 'LEGATE', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-[#F47325]'
+          <Route path="legate" element={<EatsEzy state={{ image:"mobile-mockup.png" ,title: 'LEGATE', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-[#F47325]'
             }} />} />
-          <Route path="express-cart" element={<EatsEzy state={{ title: 'Express Cart', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-[#F7BE47]'
+          <Route path="express-cart" element={<EatsEzy state={{ image:"mobile-mockup.png" ,title: 'Express Cart', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-[#F7BE47]'
             }} />} />
-          <Route path="you-adme" element={<EatsEzy state={{ title: 'YOU adme', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-[#FDCF09]'
+          <Route path="you-adme" element={<EatsEzy state={{ image:"mobile-mockup.png" ,title: 'YOU adme', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-[#FDCF09]'
             }} />} />
-          <Route path="orizon-motorcycles" element={<EatsEzy state={{ title: 'Orizon Motorcycles', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-[#F2994A]'
+          <Route path="orizon-motorcycles" element={<EatsEzy state={{ image:"mobile-mockup.png" ,title: 'Orizon Motorcycles', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-[#F2994A]'
             }} />} />
-          <Route path="osma" element={<EatsEzy state={{ title: 'OFMS', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-[#F79635]'
+          <Route path="osma" element={<EatsEzy state={{ image:"mobile-mockup.png" ,title: 'OFMS', shortDesc: "Mobile App Design & Developement", Description: "Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum ipsum is dummy text.Loreum " ,bg:'bg-[#F79635]'
             }} />} />
         </Route>
       </Routes>
--- src/components/ExploreWorks/EatsEzy.js
+++ src/components/ExploreWorks/EatsEzy.js
@@ -4,14 +4,14 @@ import { useLocation } from 'react-router-dom';
 export default function EatsEzy({state}) {
     
     // const location = useLocation()
-    const [showGif, setShowGif] = useState("etzezy_animation.gif")
+    // const [showGif, setShowGif] = useState("etzezy_animation.gif")
     // console.log(location)
-    useEffect(() => {
-        setTimeout(() => {
-            console.log('This will run after 2 second!')
-            setShowGif("eatsezy-mockup.png")
-        }, 2300);
-    }, []);
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         console.log('This will run after 2 second!')
+    //         setShowGif("eatsezy-mockup.png")
+    //     }, 2300);
+    // }, []);
     return (
         <div className={`${state.bg} flex max-w-[75vw]`}>
             <div className='text-white flex flex-col mx-auto justify-center pl-5'>
@@ -19,7 +19,7 @@ export default function EatsEzy({state}) {
                 <p className='text-xl mt-4 mb-5 font-semibold'>{state.shortDesc}</p>
                 <p className='text-xl font-semibold'>{state.Description}</p>
             </div>
-            <img className='w-[45vw] h-fit m-auto' src={showGif} alt="" srcset="" />
+            <img className='w-1/2 m-auto' src={state.image} alt="" srcset="" />
         </div>
     )
 }
